b9f70debce08a2a5540c5547a165942b769c1b76
JASON_LUI <Jason53lui@gmail.com>
2023-11-04 01:03:44 -0700

    changed the strings to be only user display message

--- searchStrings.js
+++
@@ -1,8 +0,0 @@
-const messages = {
-    invalidInput: "Please enter valid word and definition (alphabet letters only)!",
-    indexHTMLString: "index.html",
-    displayArea: "displayArea",
-    searchTerm: "searchTerm",
-    getString: "GET",
-    endPointRoot: "https://briankjkim.com/isalab6/api/v1/definition/"
-};
---
+++ utility/storeString.js
@@ -0,0 +1,8 @@
+const storeStrings = {
+    message: "Please enter valid word and definition (alphabet letters only)!",
+    ERROR_LANGUAGE_MESSAGE: "Error fetching languages:",
+    ENTRY_NOT_FOUND_MESSAGE: "Entry not found.",
+};
+
+
+
