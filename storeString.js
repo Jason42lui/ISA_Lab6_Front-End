77aff9f41523629f3c33161185145a30992c017e
JASON_LUI <Jason53lui@gmail.com>
2023-11-03 19:11:31 -0700

    made all strings in the HTML into a seperate String folder"

---
+++ storeString.js
@@ -0,0 +1,26 @@
+const storeStrings = {
+    message: "Please enter valid word and definition (alphabet letters only)!",
+    indexHTMLString: "index.html",
+    CONTENT_TYPE_STRING: "Content-Type",
+    APPLICATION_JSON_STRING: "application/json",
+    POST_STRING: "POST",
+    PATCH_STRING: "PATCH",
+    DELETE_STRING: "DELETE",
+    GET_STRING: "GET",
+    FEED_BACK_STRING: "feedback",
+    WORD_STRING: "word",
+    DEFINITION_STRING: "definition",
+    WORD_LANGUAGE_STRING: "wordLanguage",
+    DEFINITION_LANGUAGE_STRING: "definitionLanguage",
+    endPointRoot: "https://briankjkim.com/isalab6/",
+    EMPTY_STRING:  "",
+    OPTION_STRING: "option",
+    ERROR_LANGUAGE_MESSAGE: "Error fetching languages:",
+    ENTRY_NOT_FOUND_MESSAGE: "Entry not found.",
+    DOM_STRING: "DOMContentLoaded",
+    SUBMIT_UPDATE_DELETE_PATH: "api/v1/definition/",
+    LANGUAGE_PATH: "api/v1/languages",
+};
+
+
+
